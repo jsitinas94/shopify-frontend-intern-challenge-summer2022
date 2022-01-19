cb375c3df048a9cf12bacedf5a1aaf945cad4956
jsitinas94 <jsitinas94@gmail.com>
2022-01-18 20:16:14 -0500

    created API GET request to fetch images

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = "https://api.nasa.gov/planetary/apod?api_key=5RvZaPtz0NBcMOI5AdoF27epQFAiY7y3lck1VJ3z"
+
+const fetchImages = async () => {
+    return axios({
+        method: "GET", 
+        url: `${API_URL}&count=10`,
+    }).then((response) => response.data)
+}
+
+export { fetchImages };
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchImages } from "../api";
 
 export default function App() {
+
+    useEffect(async() => {
+        const imageData = await fetchImages();
+        setImageList(imageData);
+        //console.log(data);
+    }, []);
+
+    const [ imageList, setImageList ] = useState([]);
+
+    console.log(imageList)
+
     return <h1>Welcome to Spacetagram</h1>
 }
 
